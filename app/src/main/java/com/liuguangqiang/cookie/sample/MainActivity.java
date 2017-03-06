83769d6d2f911419bb57114f143e96b7a59051e1
Eric <guangqiang.dev@gmail.com>
2017-03-06 22:17:01 +0800

    Add license into README.md

--- app/src/main/java/com/liuguangqiang/cookie/sample/MainActivity.java
+++ app/src/main/java/com/liuguangqiang/cookie/sample/MainActivity.java
@@ -1,6 +1,5 @@
 package com.liuguangqiang.cookie.sample;
 
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
