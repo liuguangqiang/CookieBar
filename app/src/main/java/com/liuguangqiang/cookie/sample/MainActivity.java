16f2d93ad622eab28de65f3a80ed185c4eea9a50
Eric <guangqiang.dev@gmail.com>
2017-11-16 13:49:18 +0800

    upgrade build tools version.

--- app/src/main/java/com/liuguangqiang/cookie/sample/MainActivity.java
+++ app/src/main/java/com/liuguangqiang/cookie/sample/MainActivity.java
@@ -1,12 +1,11 @@
 package com.liuguangqiang.cookie.sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
 import com.liuguangqiang.cookie.CookieBar;
 import com.liuguangqiang.cookie.OnActionClickListener;
 
